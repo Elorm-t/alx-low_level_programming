1703c74784c61019f976ec0cedf28e6ab80259c2
Elorm-t <delimits@ymail.com>
2023-02-16 03:38:19 -0800

    Task 6, get sizes of various types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,11 +11,11 @@
 
 int main(void)
 {
-	puts("Size of a char: %d byte(s)", sizeof(char));
-	puts("Size of an int: %d bytes(s)", sizeof(int));
-	puts("Size of a long int: %d byte(s)", sizeof(long int));
-	puts("Size of a long long int: %d byte(s)", sizeof(long long int));
-	puts("Size of a float: %d byte(s)", sizeof(float));
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d bytes(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
 
 	return (0);
 }
