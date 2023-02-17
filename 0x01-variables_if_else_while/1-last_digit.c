6c8a9192b493d4aeca3ab579293f3441a3a52464
Elorm-t <delimits@ymail.com>
2023-02-17 00:53:19 -0800

    Task 1, determine last digit of randomly generated number

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Program determines the last digit of a randomly generated
+ * number and outputs a corresponding message based on value of last digit.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n, l_digit;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	l_digit = n % 10;
+
+	if (l_digit > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, l_digit);
+	else if (l_digit == 0)
+		printf("Last digit of %d is %d and is 0\n", n, l_digit);
+	else
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l_digit);
+
+	return (0);
+
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,3 +1,5 @@
 Project - 0x01-variables_if_else_while
 
 Task 0 - Positive anything is better than negative nothing: Program checks and outputs if a randomly generated number is positive, negative or zero.
+
+Task 1 - The last digit: Program determines the last digit of a randomly generated number and outputs a message based on its value.
