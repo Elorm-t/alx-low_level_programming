7a60582c1b06e4fc715e83cbc6046133e41e6bd5
Elorm-t <delimits@ymail.com>
2023-02-21 03:35:56 -0800

    Task 0, outputs specified string using custom function

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,26 @@
+#include "alx.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: Program prints a specified string using a custum function.
+ *
+ * Return: Always 0 (Success).
+ */
+
+int main(void)
+{
+	char str[] = "_putchar";
+	int i = 0;
+	int str_len = sizeof(str) / sizeof(char);
+
+	while (i < str_len)
+	{
+		_putchar(str[i]);
+		i++;
+	}
+
+	_putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+Project - 0x02-functions_nested_loops
+
+Task 0 - _putchar: Program prints a specified string followed by newline using a custom function.
