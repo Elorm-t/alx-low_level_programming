7bc07d5ddc47fafe887798f4bd0befe63e5afc08
Elorm-t <delimits@ymail.com>
2023-02-23 13:12:07 -0800

    Task 12, print integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -14,11 +14,8 @@ void print_number(int n)
 		n = -n;
 	}
 
-	if (n != 0)
-	{
+	if (n / 10)
 		print_number(n / 10);
-		_putchar((n % 10) + '0');
-	}
-	else
-		_putchar('0');
+
+	_putchar((n % 10) + '0');
 }
