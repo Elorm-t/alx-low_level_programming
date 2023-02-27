5083999a9929bf0ef8f6a37814e316d42352437a
Elorm-t <delimits@ymail.com>
2023-02-27 00:16:24 -0800

    Task 0, update value using pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function updates the value it point to.
+ * @n: pointer to variable to be updated
+ *
+ * Return: void
+ */
+
+void reset_to_98(int *n);
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+Project - 0x05-pointers_arrays_strings
+
+Task 0 - 98 Battery st.: A function that takes a pointer to an int as parameter and updates the value it points to to 98.
