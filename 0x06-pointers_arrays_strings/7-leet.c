5e4500f7ddfd97173b0148acab0238a4f3f7c392
Elorm-t <delimits@ymail.com>
2023-03-01 05:25:17 -0800

    Task 7, encode string into 1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,36 +9,22 @@
 
 char *leet(char *str)
 {
-	int i;
+	int i = 0, j;
+	int ch[] = {'a', 'e', 'o', 't', 'l'};
+	int nums[] = {'4', '3', '0', '7', '1'};
 
-	for (i = 0; str[i] != '\0'; i++)
+	while (str[i] != '\0')
 	{
-		switch (str[i])
+		for (j = 0; j < 5; j++)
 		{
-		case 'a':
-		case 'A':
-			str[i] = 4;
-			break;
-		case 'e':
-		case 'E':
-			str[i] = 3;
-			break;
-		case 'o':
-		case 'O':
-			str[i] = 0;
-			break;
-		case 't':
-		case 'T':
-			str[i] = 7;
-			break;
-		case 'l':
-		case 'L':
-			str[i] = 1;
-			break;
-		default:
-			break;
+			if (str[i] = ch[j] || str[i] == (ch[j] - 32))
+			{
+				str[i] = nums[j];
+				break;
+			}
 		}
-
-		return (str);
+		i++;
 	}
+
+	return (str);
 }
