35eaf75d2fbe63640156906312ca1d3f5a1fabb4
Elorm-t <delimits@ymail.com>
2023-02-28 23:48:59 -0800

    Task 0, concatenate strings at n bytes

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * *_strncat - concatenates strings using n bytes from src
+ * @dest: destination character pointer
+ * @src: source character pointer
+ * @n: number of bytes
+ *
+ * Return: character pointer of concatenated string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0, j;
+
+	while (dest[i] != '\0')
+		i++;
+
+	for (j = 0; src[j] != '\n' && n > 0; j++, i++, n--)
+		dest[i] = src[j];
+
+	return (dest);
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -1,3 +1,5 @@
 Project - 0x06-Pointers_arrays_strings
 
 Task 0 - strcat: A function that concatenates two strings.
+
+Task 1 - strncat: A function that concatenates two strings using at most n bytes form src.
