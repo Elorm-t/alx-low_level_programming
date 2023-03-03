0b23ea9b9ba4ddf9e8edb605cdc0b6b0eed98e8a
Elorm-t <delimits@ymail.com>
2023-03-03 04:10:24 -0800

    Task 3, get length of prefix substring

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strspn - gets length of prefix substring
+ * @s: character pointer to string
+ * @accept: character pointer to prefix substring
+ *
+ * Return: numbers of bytes in s
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j;
+	unsigned int res = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] = '\0', j++)
+		{
+			if (s[i] == accept[j])
+			{
+				res++;
+				break;
+			}
+		}
+		if (accept [j] == '\0')
+			break;
+	}
+
+	return (res);
+}
--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -5,3 +5,5 @@ Task 0 - memset: A function that fills memory with a constant byte.
 Task 1 - memcpy: A function that copies a memory area.
 
 Task 2 - strchr: A function that locates a character in a string.
+
+Task 3 - strspn: A function that gets the length of a prefix substring.
