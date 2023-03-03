95fffd543978933bf31553407795d9bc62080ab4
Elorm-t <delimits@ymail.com>
2023-03-03 03:00:59 -0800

    Task 0, fill memory with n bytes

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *_memset - fill memory with constant byte.
+ * @s: character pointer of memory location
+ * @b: value to be copied to memory block
+ * @n: number of bytes in memory block to be set
+ *
+ * Return: pointer to the memory area s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+Project - 0x07-pointers_arrays_strings
+
+Task 0 - memset: A function that fills memory with a constant byte.
