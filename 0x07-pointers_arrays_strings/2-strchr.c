0345e554dc94563c34de26fc8404926cc4377a3f
Elorm-t <delimits@ymail.com>
2023-03-03 03:35:10 -0800

    Task 2, locate character in string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * *_strchr - locates a character in a string
+ * @s: character pointer to string
+ * @c: character to be located in string
+ *
+ * Return: first occurrence of character c in string
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0, s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+			return (s + i);
+	}
+
+	if (s[i] == c)
+		return (s + i);
+
+	return (NULL);
+}
--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -3,3 +3,5 @@ Project - 0x07-pointers_arrays_strings
 Task 0 - memset: A function that fills memory with a constant byte.
 
 Task 1 - memcpy: A function that copies a memory area.
+
+Task 2 - strchr: A function that locates a character in a string.
