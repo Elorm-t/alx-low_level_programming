8dff8e2c765daebcb28c23813bfad42d309961be
Elorm-t <delimits@ymail.com>
2023-03-04 10:18:47 -0800

    Task 5, locate substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * *_strstr - locates a substring
+ * @haystack: character pointer to string to search through
+ * @needle: character pointer of substring to search for.
+ *
+ * Return: character pointer to beginning of located substring,
+ * or NULL if not found.
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j, k = 0;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		j = 0;
+		while (haystack[i + k] != '\0' && needle[j + k] != '\0'
+			&& haystack[i + k] == needle[j + k])
+		{
+			if (haystack[i + k] != needle[j + k])
+				break;
+			k++;
+		}
+		if (needle[j + k]) == '\0'
+			return (haystack + i);
+		j++;
+	}
+
+	return ('\0');
+}
--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -9,3 +9,5 @@ Task 2 - strchr: A function that locates a character in a string.
 Task 3 - strspn: A function that gets the length of a prefix substring.
 
 Task 4 - strpbrk: A function that searches a string for any of a set of bytes.
+
+Task 5 - strstr: A function that locates a substring.
