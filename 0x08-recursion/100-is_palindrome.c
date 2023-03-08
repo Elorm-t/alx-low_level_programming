adb9e40ee555973afee49c99cfdd8167157b8d2f
Elorm-t <delimits@ymail.com>
2023-03-08 08:50:23 -0800

    Task 7, check palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,57 @@
+#include "main.h"
+
+/**
+ * str_len - gets length of string
+ * @str: charactere pointer to string
+ *
+ * Return: length of string
+ */
+
+int str_len(char *str)
+{
+	int ctr;
+
+	if (*s == '\0')
+		return (0);
+
+	ctr = 1 + str_len(s + 1);
+
+	return (ctr);
+}
+
+/**
+ * helper_fn - iterate recursively to compare charaters of string
+ * @str: character pointer to string
+ * @l: beginning index of string
+ * @r: end index of string
+ *
+ * Return: 0 if not palindrome, 1 if palindrome
+ */
+
+int helper_fn(char *str, int l, int r)
+{
+	if (l >= r)
+		return (1);
+	if (str[l] == str[r])
+		return (helper_fn(str, l + 1, r - 1));
+
+	return (0);
+}
+
+/**
+ * is_palindrome - check if string is palindrome
+ * @s: character pointer to string
+ *
+ * Return: 0 if not palindrome, 1 if palindrome
+ */
+
+int is_palindrome(char *s)
+{
+	int len = str_len(s);
+	int i = 0;
+
+	if (len == 0 || len == 1)
+		return (1);
+	else
+		return (helper_fn(s, i, len - 1));
+}
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -13,3 +13,5 @@ Task 4 - Once an idea has taken hold of the brain it's almost impossible to erad
 Task 5 - Your subconscious is looking for the dreamer: A function that returns the natural square root of a number.
 
 Task 6 - Inception. Is it possible?: A  function that returns 1 if the input integer is a prime number, otherwise return 0.
+
+Task 7 - They say we only use a fraction of our brain's true potential: A function that returns 1 if a string is a palindrome and 0 if not.
