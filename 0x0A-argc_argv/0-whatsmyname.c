34f594ed309bc3c2fd53d331c92c1fbf08f89ba0
Elorm-t <delimits@ymail.com>
2023-03-10 03:32:30 -0800

    Task 0, print program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argument vector, which is an array of all arguments passed to the
+ * program
+ *
+ * Description: A program that prints its name.
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(argc, *argv[])
+{
+	if (argc == 1)
+		printf("%s\n", argv[argc - 1]);
+	else
+		printf("Program not expecting additional arguments.\n");
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,3 @@
+Project - 0x0A-argc_argv
+
+Task 0 - It ain't what they call you, it's what you answer to: A program that prints its name.
