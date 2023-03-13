53cd5f4f8733e36e8472aa93a894610ba92b8b9d
Elorm-t <delimits@ymail.com>
2023-03-13 06:09:13 -0700

    Task 0, create character array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * *create_array - create an array of chars, initialized with a specific
+ * char
+ * @size: size of the array
+ * @c: specified character
+ *
+ * Return: NULL if size is 0, pointer to the array, or null if it fails
+ */
+
+char  *create_array(unsigned int size, char c)
+{
+	char *arr;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+
+	arr = malloc(sizeof(char) * size);
+
+	if (arr == 0)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		arr[i] = c;
+
+	return (arr);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,3 @@
+Project - 0x0B-malloc_free
+
+Task 0 - Float like a butterfly, sting like a bee: A function that creates an array of chars, and initializes it with a specific char.
