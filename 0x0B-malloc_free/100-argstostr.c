5ed45e04031245fbae6fcc9add5cc5027218c539
Elorm-t <delimits@ymail.com>
2023-03-14 13:25:36 -0700

    Task 5, concatenate program arguments

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * argstostr - concatenates all arguments of a program
+ * @ac: argument count
+ * @av: argument vector, i.e., array of arguments
+ *
+ * Return: NULL is ac is 0 or av is NULL, character pointer to
+ * concatenated string, or NULL if it fails.
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int i, j, k, size;
+	char *str;
+
+	i = j = k = 0;
+
+	if (ac < 1)
+		return (NULL);
+
+	for (; i < ac; i++)
+		for (; av[i][j] != '\0'; j++)
+			size++;
+
+	size += ac + 1;
+
+	str = malloc(sizeof(char) * size);
+	if (str == 0)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			str[k] = av[i][j];
+			k++;
+		}
+		str[k] = '\n';
+		k++;
+	}
+
+	str[k] = '\0';
+
+	return (str);
+}
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -9,3 +9,5 @@ Task 2 - He who is not courageous: A function that concatenates two strings.
 Task 3 - If you even dream of beating me: A function that returns a pointer to a 2 dimensional array of integers.
 
 Task 4 - It's not bragging if you can back it up: A function that frees a 2 dimensional grid previously created.
+
+Task 5 - It isn't the mountains ahead: A function that concatenates all the arguments of a program.
