ee0df0f831bf7b3a85b605bdd6fd44fba170b1d2
Elorm-t <delimits@ymail.com>
2023-03-16 10:08:32 -0700

    Task 3, create array of integers

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * array_range - creates an array of integers
+ * @min: minimum value
+ * @max: maximum value
+ *
+ * Return: NULL if min is greater than max, malloc fails, or pointer
+ * to array if successful.
+ */
+
+int *array_range(int min, int max)
+{
+	int i;
+	int *ptr;
+
+	if (min > max)
+		return (NULL);
+
+	ptr = malloc(sizeof(int) * (max - min + 1));
+
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; min <= max; i++, min++)
+		ptr[i] = min;
+
+	return (ptr);
+}
--- 0x0C-more_malloc_free/README.md
+++ 0x0C-more_malloc_free/README.md
@@ -5,3 +5,5 @@ Task 0 - Trust no one: A function that allocates memory using malloc.
 Task 1 - string-nconcat: A function that concatenates two strings.
 
 Task 2 - A function that allocates memory for an array, using malloc.
+
+Task 3 - A function that creates an array of integers.
