276085de9e24c22f5a5ac198ed340304728c21f9
Elorm-t <delimits@ymail.com>
2023-03-20 14:08:46 -0700

    Task 4, create new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,54 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - creates new dog
+ * @name: character pointer to dog name
+ * @age: variable to hold age of dog
+ * @owner: character pointer to owner of dog
+ *
+ * Return: NULL if function fails, else new dog
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	int i = 0, j = 0, k;
+	char *n_cpy, *o_cpy;
+	dog_t *n_dog;
+
+	n_dog = malloc(sizeof(dog_t));
+
+	if (n_dog == NULL)
+		return (NULL);
+
+	while (name[i] != '\0')
+		i++;
+
+	while (owner[j] != '\0')
+		j++;
+
+	n_cpy = malloc(sizeof(char) * i + 1);
+	if (n_cpy == NULL)
+	{
+		free(n_dog);
+		return (NULL);
+	}
+
+	o_cpy = malloc(sizeof(char) * j + 1);
+	if (o_cpy == NULL)
+	{
+		free(n_cpy);
+		free(n_dog);
+		return (NULL);
+	}
+	for (k = 0; k <= i; k++)
+		n_cpy = name[k];
+	for (k = 0; k <= j; k++)
+		o_cpy = owner[k];
+
+	n_dog->name = n_cpy;
+	n_dog->age = age;
+	n_dog->owner = o_cpy;
+
+	return (n_dog);
+}
--- 0x0E-structures_typedef/README.md
+++ 0x0E-structures_typedef/README.md
@@ -5,3 +5,7 @@ Task 0 - Poppy: Creating a header file with a new type struct dog.
 Task 1 - A dog is the only thing on earth that loves you more than you love yourself: A function that initialize a variable of type struct dog.
 
 Task 2 - A dog will teach you unconditional love.: A function that prints a struct dog.
+
+Task 3 - Outside of a dog, a book is a man's best friend.: Define a new type dog_t.
+
+Task 4 - A door is what a dog is perpetually on the wrong side of: A function that creates a new dog.
