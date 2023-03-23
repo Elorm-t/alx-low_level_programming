95ec739910bdbc2a0ef9f5088ef02d194b7e93df
Elorm-t <delimits@ymail.com>
2023-03-23 11:28:24 -0700

    Task 1, print numbers in variable argument

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints numbers
+ * @separator: the string to be printed between numbers
+ * @n: number of integers passed to function
+ *
+ * Return: void
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list parg;
+
+	va_start(parg, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(parg, int));
+		if (i != (n - 1) && separator != NULL)
+			print("%s", separator);
+	}
+
+	printf("\n");
+
+	va_end(list);
+}
--- 0x10-variadic_functions/README.md
+++ 0x10-variadic_functions/README.md
@@ -1,3 +1,5 @@
 Project - 0x10-variadic_functions
 
 Task 0 - Beauty is variable, ugliness is constant: A function that returns the sum of all its parameters.
+
+Task 1 - To be is to be the value of a variable: A function that prints numbers, followed by a new line.
